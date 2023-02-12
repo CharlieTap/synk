b92df6b76d7004462e1ac70f3b62f34deba4023b
CharlieTap <charlie.tapping@deliveroo.co.uk>
2023-02-12 21:52:58 +0000

    bump version

--- delight-metastore/build.gradle.kts
+++ delight-metastore/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-//                implementation(projects.synk)
+                implementation(projects.synk)
                 implementation(projects.concurrentMap)
                 implementation(libs.murmurhash)
                 implementation(libs.androidx.collections.kmp)
