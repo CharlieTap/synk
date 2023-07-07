134f2bb5647a262ddeac2e8a5a3d7fd25c6e5dc6
CharlieTap <charlie.tapping@deliveroo.co.uk>
2023-07-07 20:25:48 +0100

    refactored annotations into separate module

---
+++ libs/annotations/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.atomic.fu)
+    id("kotlinter-conventions")
+    id("maven-publish")
+}
+group = "com.tap.synk.lib"
+version = libs.versions.version.name.get()
+
+kotlin {
+
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.java.compiler.version.get().toInt()))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+    }
+
+    targets {
+        jvm()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlinx.atomic.fu)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = libs.versions.java.bytecode.version.get()
+}
